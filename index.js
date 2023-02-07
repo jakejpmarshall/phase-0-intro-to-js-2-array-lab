f721289957c0395d3deb5ceebe21df471f9c684c
jakejpmarshall@gmail.com <jakemarshall@Jacobs-MacBook-Air.local>
2023-02-07 13:57:35 -0700

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+let newCats;
+function appendCat(name) {
+   return newCats = [...cats, name];
+}
+appendCat('Broom');
+
+function prependCat(name) {
+    return newCats = [name, ...cats];
+}
+prependCat("Arnold");
+
+function removeLastCat() {
+    return newCats = cats.slice(0, -1)
+}
+removeLastCat();
+
+function removeFirstCat() {
+    return newCats = cats.slice(1)
+}
+removeFirstCat
